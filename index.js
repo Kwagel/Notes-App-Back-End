1cb992b35b5af53a3c3cbef65958f6aecf59ee24
Kwagel <92309850+Kwagel@users.noreply.github.com>
2022-08-29 09:09:07 +0100

    front end integrated

--- index.js
+++ index.js
@@ -2,23 +2,26 @@ const express = require('express')
 const {response} = require("express");
 const app = express()
 const cors = require('cors')
-
+app.use(express.static('build'))
 app.use(cors())
 let notes = [
     {
         id: 1,
+        title: "note 3",
         content: "HTML is easy",
         date: "2022-05-30T17:30:31.098Z",
         important: true
     },
     {
         id: 2,
+        title: "note 3",
         content: "Browser can execute only Javascript",
         date: "2022-05-30T18:39:34.091Z",
         important: false
     },
     {
         id: 3,
+        title: "note 3",
         content: "GET and POST are the most important methods of HTTP protocol",
         date: "2022-05-30T19:20:14.298Z",
         important: true
@@ -77,6 +80,7 @@ app.post('/api/notes', (request, response) => {
 
     const note = {
         id: generateId(),
+        title: body.title,
         content: body.content,
         date: new Date(),
         important: body.important || false,
